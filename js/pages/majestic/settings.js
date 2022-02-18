58840b50650d3409b8ac94627f5d308ab282820d
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2022-02-19 02:25:03 +0600

    it helps to move to membership section initially when membership edited, added etc

--- js/pages/majestic/settings.js
+++ js/pages/majestic/settings.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	$('.profile-section').hide();
 	$('.profile-section:first').show();
+	if($('#memberships').attr('data-show-initially') == '1'){
+		$('.profile-section').hide();
+		$('#memberships').show();
+	}
 	$('.profile-nav-link').on('click',function(){
 		let section = $(this).attr('id').substr(4);
 		console.log(section);
