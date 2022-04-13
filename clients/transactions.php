aed18f95327c19e308607f1d5399e89fa398d796
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2022-04-14 04:14:05 +0600

    it adds the transactions content in clients and vendors as milestone 10 demand

--- clients/transactions.php
+++ clients/transactions.php
@@ -26,14 +26,81 @@ if(!$isClientLoggedIn){
 		MyHelpers::includeWithVariables('../layouts/top_nav.php', ['isClientLoggedIn' => $isClientLoggedIn], $print = true);
 		?>
 		<div class="main-body-content">
-            <section>
+			<section class="section-type-01">
+                <div class="ad-space-container-160">
+                    <div class="ad-space-type01-desktop">
+                        <!-- Ad Space (160 x 600) -->
+                    </div>
+                    <div class="ad-space-type01-desktop">
+                        <!-- Ad Space (160 x 600) -->
+                    </div>
+                </div>
+                <!-- <div class="ad-space-type01-mobile">
+
+                </div> -->
                 <div class="content-container-center">
-					<div class="fix half floatleft"><h1 class="fs_30 lh_40 font_bold text_dark_ash mb_10 pl_5 pr_5">Transactions</h1></div>
-					<div class="fix half floatleft"></div>
-					
-					
-				</div>
-			</section>
+                    <div class="vendor-profile-container">
+                        <div class="vendor-profile-header-container">
+                        </div>
+                        <div class="profile-section">
+                            <h2>Transactions</h2>
+                            <div class="custom-table-container-01">
+                                <div class="custom-table-heading-01">
+                                    <div class="table-row-content-container-date">
+                                        date
+                                    </div>
+                                    <div class="table-row-content-container-vendor">
+                                        vendor
+                                    </div>
+                                    <div class="table-row-content-container-item">
+                                        item
+                                    </div>
+                                    <div class="table-row-content-container-qty">
+                                        quantity
+                                    </div>
+                                    <div class="table-row-content-container-price">
+                                        price
+                                    </div>
+                                </div>
+                                <div class="custom-table-row-01">
+                                    <div class="table-row-content-container-date">
+                                        09/28/22
+                                    </div>
+                                    <div class="table-row-content-container-vendor">
+                                        Music Man Stan
+                                    </div>
+                                    <div class="table-row-content-container-item">
+                                        Lorem ipsum dolor...
+                                    </div>
+                                    <div class="table-row-content-container-qty">
+                                        2
+                                    </div>
+                                    <div class="table-row-content-container-price">
+                                        $8,995.00
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="spacer-10px"></div>
+                            <div class="spacer-10px"></div>
+                            <div class="pagination-container">
+                                <div class="pagination-link-current">1</div>
+                                <a href="" class="white-text"><div class="pagination-link">2</div></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="ad-space-type01-mobile">
+
+                </div> -->
+                <div class="ad-space-container-160">
+                    <div class="ad-space-type01-desktop">
+                        <!-- Ad Space (160 x 600) -->
+                    </div>
+                    <div class="ad-space-type01-desktop">
+                        <!-- Ad Space (160 x 600) -->
+                    </div>
+                </div>
+            </section>
 		</div>
 		<?php 
         MyHelpers::includeWithVariables('../layouts/footer.php', [], $print = true);
